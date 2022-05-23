03122320a53ef8ef649f422dd513d704dde5fcb9
jackiepiepkorn <54698440+jackiepiepkorn@users.noreply.github.com>
2022-05-22 23:09:49 -0700

    Created 3 tests for the snippet codes

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,27 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 public class MarkdownParseTest {
-
+    
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void snippet1() throws IOException {
+        
+        String content = Files.readString(Path.of("snippet1.md");
+        assertEquals(List.of("`google.com"), MarkdownParse.getLinks(content);
+    }
+                 
+    @Test
+    public void snippet2() throws IOException {
+        
+        String content = Files.readString(Path.of("snippet2.md");
+        assertEquals(List.of("a.com", "a.com(())", "example.com"), MarkdownParse.getLinks(content);
+    }
+                     
+    @Test
+    public void snippet3() throws IOException {
+        
+        String content = Files.readString(Path.of("snippet3.md");
+        assertEquals(List.of("https://www.twitter.com", "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedu",
+                             "https://cse.ucsd.edu/"), MarkdownParse.getLinks(content);
     }
 
 }
